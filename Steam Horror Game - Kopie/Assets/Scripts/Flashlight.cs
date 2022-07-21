99089037c1e52c614fa049733c70f76a536d1bcb
DTRIX05 <djgarcia@gmx.de>
2022-07-21 20:09:36 +0200

    Flashlight added

---
+++ Steam Horror Game - Kopie/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Flashlight : NetworkBehaviour
+{
+    public GameObject flashlight;
+
+    public AudioSource turnOn;
+    public AudioSource turnOff;
+
+    public bool on;
+    public bool off;
+    
+    void Start()
+    {
+        off = true;
+        flashlight.SetActive(false);
+    }
+    void Update()
+    {
+        if(off && Input.GetButtonDown("F"))
+        {
+            flashlight.SetActive(true);
+            turnOn.Play();
+            off = false;
+            on = true;
+        }
+        else if (on && Input.GetButtonDown("F"))
+        {
+            flashlight.SetActive(false);
+            turnOff.Play();
+            off = true;
+            on = false;
+        }
+    }
+}
